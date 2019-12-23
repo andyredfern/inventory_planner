03b37dbb4b63982751a5ab661eb133ea4776d874
Andy Redfern <andy@redfern.it>
2019-12-23 22:54:00 +0000

    Developing initial tests - work in progress

--- tests/PurchaseOrderTest.php
+++ tests/PurchaseOrderTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Aredfern\Invplan;
+use Aredfern\Invplan\Models\InvPlanAPI;
 
 class PurchaseOrderTest extends \PHPUnit\Framework\TestCase
 {
@@ -8,13 +9,31 @@ class PurchaseOrderTest extends \PHPUnit\Framework\TestCase
     protected $PurchaseOrder;
 
     public function setUp():void {
-        $this->PurchaseOrder = new Models\PurchaseOrder();
+
+        $token = "123456789";
+        $account = "987654321";
+
+        $response = array("purchase-order"=>array("type"=>"po"));
+
+        $api = $this->getMockBuilder('Aredfern\Invplan\Models\InvPlanAPI')
+            ->setConstructorArgs(array($token, $account))
+            ->getMock();
+
+        $api->expects($this->once())
+            ->method('getResource')
+            ->will($this->returnValue($response));
+
+        $this->PurchaseOrder = new Models\PurchaseOrder($api);
     }
 
     /** @test  */
     public function get_a_purchase_order_that_exists() {
-        $this->PurchaseOrder->getById(1);
-        $this->assertEquals($this->PurchaseOrder->getById(1), 1);
+        $testArray = $this->PurchaseOrder->getById(1);
+        $this->assertIsArray($testArray);
+        $this->assertArrayHasKey("purchase-order",$testArray);
     }
+
+    
+
 
 }
