553c00220d6cc79c5c2309eed87528817fcbafa0
Andy Redfern <andy@redfern.it>
2020-01-08 22:12:12 +0000

    puchaseOrders getCollection completed

--- app/Models/PurchaseOrders.php
+++ app/Models/PurchaseOrders.php
@@ -18,9 +18,12 @@ class PurchaseOrders
     }
 
 
-    public function get($fields=null,$sort=null,$limit=null,$page=null) {  
-        
-        $returned = $this->interface->getResource("purchase-orders/".$id);
+    public function getCollection($fields=null,$sort_field=null,$sort_direction=null,$limit=null,$page=null,$filter=null) {  
+        $filter_url = http_build_query($filter);
+        echo $filter_url."<br>";
+        $url =  "purchase-orders?fields=" . $fields . "&" .$sort_field."=".$sort_direction."&limit=".$limit."&page=".$page."&".$filter_url;
+        echo $url;
+        $returned = $this->interface->getResource($url);
         return $returned;
     }
 
